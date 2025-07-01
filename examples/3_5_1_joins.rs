9b38785999f0f9eee444ee258fb05342e4f534a0
Eric Fecteau <eric.m.fecteau@gmail.com>
2025-07-01 08:57:09 -0400

    Maintainable

--- examples/3_5_1_joins.rs
+++ examples/3_5_1_joins.rs
@@ -9,139 +9,124 @@ fn main() {
         let mm = format!("{:02}", m);
 
         let args = ScanArgsParquet::default();
-        let lf = LazyFrame::scan_parquet(format!("./data/lfs_parquet/pub{mm}23.parquet"), args)
-            .unwrap()
+        let lf =
+            LazyFrame::scan_parquet(format!("./data/lfs_parquet/pub{mm}23.parquet"), args).unwrap();
+
+        lfs_month.push(lf);
+    }
+
+    // Concatonate vertically two (or more) datasts
+    let lf_jan_to_apr = concat(
+        [
+            lfs_month[0].clone(), // Cloned, since we need it later
+            lfs_month[1].clone(),
+            lfs_month[2].clone(),
+            lfs_month[3].clone(),
+        ],
+        UnionArgs::default(),
+    )
+    .unwrap();
+
+    // See `survmnth` going from 1 to 4 for 2023
+    println!("{}", lf_jan_to_apr.collect().unwrap());
+
+    // Update the lfs_month vector to remove variables and update values
+    for m in 1..5 {
+        let mm = format!("{:02}", m);
+
+        lfs_month[m - 1] = lfs_month[m - 1]
+            .clone()
             .filter(col("hrlyearn").is_not_null())
             .select([
                 col("rec_num"),
                 col("survyear"),
                 col("hrlyearn").alias(format!("earn_{mm}")),
             ]);
-
-        lfs_month.push(lf.collect().unwrap());
     }
 
-    println!("{:?}", lfs_month);
+    // Left join (creating a cohort)
+    let jan_cohort = lfs_month[0]
+        .clone()
+        .drop([col("survyear")])
+        .left_join(
+            lfs_month[1].clone().drop([col("survyear")]),
+            col("rec_num"),
+            col("rec_num"),
+        )
+        .left_join(
+            lfs_month[2].clone().drop([col("survyear")]),
+            col("rec_num"),
+            col("rec_num"),
+        )
+        .left_join(
+            lfs_month[3].clone().drop([col("survyear")]),
+            col("rec_num"),
+            col("rec_num"),
+        );
 
-    // // Concatonate vertically two (or more) datasts
-    // let lf_jan_to_apr = concat(
-    //     [
-    //         lf_jan.clone(), // Cloned, since we need it later
-    //         lf_feb.clone(),
-    //         lf_mar.clone(),
-    //         lf_apr.clone(),
-    //     ],
-    //     UnionArgs::default(),
-    // )
-    // .unwrap();
+    println!("{}", jan_cohort.collect().unwrap());
 
-    // // See `survmnth` going from 1 to 4 for 2023
-    // println!("{}", lf_jan_to_apr.collect().unwrap());
+    // Inner join (creating a "always earning" cohort)
+    let longitudinal_all = lfs_month[0]
+        .clone()
+        .drop([col("survyear")])
+        .inner_join(
+            lfs_month[1].clone().drop([col("survyear")]),
+            col("rec_num"),
+            col("rec_num"),
+        )
+        .inner_join(
+            lfs_month[2].clone().drop([col("survyear")]),
+            col("rec_num"),
+            col("rec_num"),
+        )
+        .inner_join(
+            lfs_month[3].clone().drop([col("survyear")]),
+            col("rec_num"),
+            col("rec_num"),
+        );
 
-    // // Get data ready for join
-    // let lf_jan = lf_jan.filter(col("hrlyearn").is_not_null()).select([
-    //     col("rec_num"),
-    //     col("survyear"),
-    //     col("hrlyearn").alias("earn_jan"),
-    // ]);
-    // let lf_feb = lf_feb.filter(col("hrlyearn").is_not_null()).select([
-    //     col("rec_num"),
-    //     col("survyear"),
-    //     col("hrlyearn").alias("earn_feb"),
-    // ]);
-    // let lf_mar = lf_mar.filter(col("hrlyearn").is_not_null()).select([
-    //     col("rec_num"),
-    //     col("survyear"),
-    //     col("hrlyearn").alias("earn_mar"),
-    // ]);
-    // let lf_apr = lf_apr.filter(col("hrlyearn").is_not_null()).select([
-    //     col("rec_num"),
-    //     col("survyear"),
-    //     col("hrlyearn").alias("earn_apr"),
-    // ]);
+    println!("{}", longitudinal_all.collect().unwrap());
 
-    // // Left join (creating a cohort)
-    // let jan_cohort = lf_jan
-    //     .clone()
-    //     .drop([col("survyear")])
-    //     .left_join(
-    //         lf_feb.clone().drop([col("survyear")]),
-    //         col("rec_num"),
-    //         col("rec_num"),
-    //     )
-    //     .left_join(
-    //         lf_mar.clone().drop([col("survyear")]),
-    //         col("rec_num"),
-    //         col("rec_num"),
-    //     )
-    //     .left_join(
-    //         lf_apr.clone().drop([col("survyear")]),
-    //         col("rec_num"),
-    //         col("rec_num"),
-    //     );
+    // More complex types of joins (e.g. join on multiple variables)
+    let fix_full_join_vars = [
+        when(col("rec_num").is_not_null())
+            .then(col("rec_num"))
+            .otherwise(col("rec_num_right"))
+            .alias("rec_num"),
+        when(col("survyear").is_not_null())
+            .then(col("survyear"))
+            .otherwise(col("survyear_right"))
+            .alias("survyear"),
+    ];
 
-    // println!("{}", jan_cohort.collect().unwrap());
+    let longitudinal_all = lfs_month[0]
+        .clone()
+        .join(
+            lfs_month[1].clone(),
+            [col("rec_num"), col("survyear")],
+            [col("rec_num"), col("survyear")],
+            JoinArgs::new(JoinType::Full),
+        )
+        .with_columns(fix_full_join_vars.clone())
+        .drop([col("rec_num_right"), col("survyear_right")])
+        .join(
+            lfs_month[2].clone(),
+            [col("rec_num"), col("survyear")],
+            [col("rec_num"), col("survyear")],
+            JoinArgs::new(JoinType::Full),
+        )
+        .with_columns(fix_full_join_vars.clone())
+        .drop([col("rec_num_right"), col("survyear_right")])
+        .join(
+            lfs_month[3].clone(),
+            [col("rec_num"), col("survyear")],
+            [col("rec_num"), col("survyear")],
+            JoinArgs::new(JoinType::Full),
+        )
+        .with_columns(fix_full_join_vars.clone())
+        .drop([col("rec_num_right"), col("survyear_right")])
+        .sort(["rec_num", "survyear"], Default::default());
 
-    // // Inner join (creating a "always earning" cohort)
-    // let longitudinal_all = lf_jan
-    //     .clone()
-    //     .drop([col("survyear")])
-    //     .inner_join(
-    //         lf_feb.clone().drop([col("survyear")]),
-    //         col("rec_num"),
-    //         col("rec_num"),
-    //     )
-    //     .inner_join(
-    //         lf_mar.clone().drop([col("survyear")]),
-    //         col("rec_num"),
-    //         col("rec_num"),
-    //     )
-    //     .inner_join(
-    //         lf_apr.clone().drop([col("survyear")]),
-    //         col("rec_num"),
-    //         col("rec_num"),
-    //     );
-
-    // println!("{}", longitudinal_all.collect().unwrap());
-
-    // // More complex types of joins (e.g. join on multiple variables)
-    // let fix_full_join_vars = [
-    //     when(col("rec_num").is_not_null())
-    //         .then(col("rec_num"))
-    //         .otherwise(col("rec_num_right"))
-    //         .alias("rec_num"),
-    //     when(col("survyear").is_not_null())
-    //         .then(col("survyear"))
-    //         .otherwise(col("survyear_right"))
-    //         .alias("survyear"),
-    // ];
-
-    // let longitudinal_all = lf_jan
-    //     .join(
-    //         lf_feb,
-    //         [col("rec_num"), col("survyear")],
-    //         [col("rec_num"), col("survyear")],
-    //         JoinArgs::new(JoinType::Full),
-    //     )
-    //     .with_columns(fix_full_join_vars.clone())
-    //     .drop([col("rec_num_right"), col("survyear_right")])
-    //     .join(
-    //         lf_mar,
-    //         [col("rec_num"), col("survyear")],
-    //         [col("rec_num"), col("survyear")],
-    //         JoinArgs::new(JoinType::Full),
-    //     )
-    //     .with_columns(fix_full_join_vars.clone())
-    //     .drop([col("rec_num_right"), col("survyear_right")])
-    //     .join(
-    //         lf_apr,
-    //         [col("rec_num"), col("survyear")],
-    //         [col("rec_num"), col("survyear")],
-    //         JoinArgs::new(JoinType::Full),
-    //     )
-    //     .with_columns(fix_full_join_vars.clone())
-    //     .drop([col("rec_num_right"), col("survyear_right")])
-    //     .sort(["rec_num", "survyear"], Default::default());
-
-    // println!("{}", longitudinal_all.collect().unwrap());
+    println!("{}", longitudinal_all.collect().unwrap());
 }
