42f1ae77780f75ab6861e2b99ff95fe00ebbdacf
Eric Fecteau <eric.m.fecteau@gmail.com>
2025-07-09 16:57:12 -0400

    Added plots

--- examples/5_0_0_plots.rs
+++ examples/5_0_0_plots.rs
@@ -1,3 +1,4 @@
+use plotlars::{BarPlot, Legend, Orientation, Plot, Rgb, ScatterPlot, Text};
 use polars::prelude::*;
 
 fn main() {
@@ -10,13 +11,94 @@ fn main() {
         .filter(col("hrlyearn").is_not_null())
         .with_column((col("hrlyearn").cast(DataType::Float64) / lit(100)).alias("hourly_wages"));
 
-    // Mean by province
-    let _df = _lf
+    // Mean by province and gender
+    let df = _lf
         .clone()
-        .group_by([col("prov")])
+        .group_by([col("gender"), col("prov")])
         .agg([col("hourly_wages")
             .mean()
             .round(2, RoundMode::HalfAwayFromZero)])
+        .with_column(col("prov").replace_strict(
+            lit(Series::from_iter(vec![
+                "10", "11", "12", "13", "24", "35", "46", "47", "48", "59",
+            ])),
+            lit(Series::from_iter(vec![
+                "NL", "PE", "NS", "NB", "QC", "ON", "MB", "SK", "AB", "BC",
+            ])),
+            None,
+            Some(DataType::String),
+        ))
+        .with_column(col("gender").replace_strict(
+            lit(Series::from_iter(vec!["1", "2"])),
+            lit(Series::from_iter(vec!["Men+", "Women+"])),
+            None,
+            Some(DataType::String),
+        ))
         .collect()
         .unwrap();
+
+    println!("{df}");
+
+    let html = BarPlot::builder()
+        .data(&df)
+        .labels("prov")
+        .values("hourly_wages")
+        .orientation(Orientation::Vertical)
+        .group("gender")
+        .colors(vec![Rgb(255, 127, 80), Rgb(64, 224, 208)])
+        .plot_title(
+            Text::from("Hourly wages by gender and province")
+                .font("Arial")
+                .size(18),
+        )
+        .x_title(Text::from("").font("Arial").size(15))
+        .y_title(Text::from("hourly_wages").font("Arial").size(15))
+        .legend_title(Text::from("gender").font("Arial").size(15))
+        .legend(
+            &Legend::new()
+                .orientation(Orientation::Horizontal)
+                .y(1.0)
+                .x(0.4),
+        )
+        .build()
+        .to_html();
+
+    let mut file = std::fs::File::create("./data/output/bar.html").unwrap();
+    std::io::Write::write_all(&mut file, html.as_bytes()).unwrap();
+
+    // Mean by gender and age (grouped)
+    let df = _lf
+        .clone()
+        .group_by([col("age_12"), col("gender")])
+        .agg([col("hourly_wages")
+            .mean()
+            .round(2, RoundMode::HalfAwayFromZero)])
+        .with_column(col("gender").replace_strict(
+            lit(Series::from_iter(vec!["1", "2"])),
+            lit(Series::from_iter(vec!["Men+", "Women+"])),
+            None,
+            Some(DataType::String),
+        ))
+        .collect()
+        .unwrap();
+
+    println!("{df}");
+
+    let html = ScatterPlot::builder()
+        .data(&df)
+        .x("age_12")
+        .y("hourly_wages")
+        .group("gender")
+        .opacity(0.5)
+        .size(12)
+        .colors(vec![Rgb(178, 34, 34), Rgb(65, 105, 225), Rgb(255, 140, 0)])
+        .plot_title("Penguin Flipper Length vs Body Mass")
+        .x_title("Body Mass (g)")
+        .y_title("Flipper Length (mm)")
+        .legend_title("Species")
+        .build()
+        .to_html();
+
+    let mut file = std::fs::File::create("./data/output/scatter.html").unwrap();
+    std::io::Write::write_all(&mut file, html.as_bytes()).unwrap();
 }
--- src/5_pub/plotting.md
+++ src/5_pub/plotting.md
@@ -1,7 +1,18 @@
 # Plotting
 
-https://github.com/plotters-rs/plotters 
-https://github.com/alceal/plotlars
+Test test
 
-vega-lite
-grisu-plot
+<div>
+<script src="https://cdn.plot.ly/plotly-2.12.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/mathjax@3.2.2/es5/tex-svg.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/mathjax@3.2.0/es5/tex-mml-chtml.js"></script>
+    
+<div id="plotly-html-element" class="plotly-graph-div" style="height:100%; width:100%;"></div>
+
+<script type="module">
+    const graph_div = document.getElementById("plotly-html-element");
+    await Plotly.newPlot(graph_div, {"data":[{"type":"bar","x":["PE","NL","QC","SK","AB","BC","ON","NB","MB","NS"],"y":[22.96,24.91,25.25,25.7,27.24,25.84,26.8,22.66,23.4,23.13],"name":"Women+","orientation":"v","marker":{"color":"rgb(255, 127, 80)"}},{"type":"bar","x":["NB","NL","BC","NS","SK","MB","PE","AB","QC","ON"],"y":[25.21,29.76,31.0,25.98,30.04,26.45,23.83,34.56,28.11,30.86],"name":"Men+","orientation":"v","marker":{"color":"rgb(64, 224, 208)"}}],"layout":{"title":{"text":"Hourly wages by gender and province","font":{"family":"Arial","size":18,"color":"rgb(0, 0, 0)"},"x":0.5,"y":0.9},"legend":{"orientation":"h","x":0.4,"y":1.0,"title":{"text":"gender","font":{"family":"Arial","size":15,"color":"rgb(0, 0, 0)"},"x":0.5,"y":0.9}},"barmode":"group"},"config":{}});
+</script>
+</div>
+
+Test test
